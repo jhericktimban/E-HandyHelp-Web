453cc8bc4cb33e148ee6ae5c33c7dd83546ca2da
Jherick Timban <jrickvillaqwe@gmail.com>
2025-02-04 19:07:38 +0800

    updated

--- web-admin/src/components/PendingHandyman.js
+++ web-admin/src/components/PendingHandyman.js
@@ -72,25 +72,25 @@ const PendingHandyman = () => {
     }
   };
 
-  const handleDeleteHandyman = (handymanId) => {
-    setHandymanToDelete(handymanId);
-    setShowDeleteModal(true);
-  };
 
-  const confirmDeleteHandyman = () => {
-    if (handymanToDelete) {
-      axios
-        .delete(`https://e-handyhelp-web-backend.onrender.com/api/handymen/${selectedHandyman._id}`)
-        .then(() => {
-          setPendingHandymen(
+
+  const handleDeleteHandyman = async () => {
+    if (selectedHandyman) {
+      try {
+        await axios.delete(
+          `https://e-handyhelp-web-backend.onrender.com/api/handymen/${selectedHandyman._id}`
+        );
+        setPendingHandymen(
             pendingHandymen.filter((handyman) => handyman._id !== selectedHandyman._id)
           );
-          setShowDeleteModal(false);
-          setAlert("Handyman deleted successfully!");
-        })
-        .catch((error) => {
-          console.error("Error deleting handyman:", error);
-        });
+        setAlert({message: "Handyman deleted successfully." });
+      } catch (error) {
+        console.error("Error deleting handyman:", error);
+        
+      } finally {
+        setShowConfirmDelete(false);
+        setSelectedHandyman(null);
+      }
     }
   };
 
@@ -121,25 +121,21 @@ const PendingHandyman = () => {
       sortable: true,
     },
     {
-      name: "Action",
-      cell: (row) => (
-        <div className="button-group">
-          <Button
-            
-            onClick={() => handleOpenModal(row)}
-            className="mb-2"
-          >
-            Detail
-          </Button>
-          <Button
-            
-            onClick={() => handleDeleteHandyman(row._id)}
-          >
-            Delete
-          </Button>
-        </div>
-      ),
-    },
+          name: "Actions",
+          cell: (row) => (
+            <div className="button-group">
+              <Button onClick={() => handleOpenModal(row)}>Details</Button>
+              <Button
+                onClick={() => {
+                  setSelectedHandyman(row);
+                  setShowConfirmDelete(true);
+                }}
+              >
+                Delete
+              </Button>
+            </div>
+          ),
+        },
   ];
 
   return (
