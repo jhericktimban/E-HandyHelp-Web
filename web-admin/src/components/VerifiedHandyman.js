0e650ffcf8d1b43cb38bf7c39b777dab64fd2daa
Jherick Timban <jrickvillaqwe@gmail.com>
2025-02-19 10:46:01 +0800

    updated

--- web-admin/src/components/VerifiedHandyman.js
+++ web-admin/src/components/VerifiedHandyman.js
@@ -12,19 +12,44 @@ const VerifiedHandyman = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [alert, setAlert] = useState(null);
 
+  const [showImageModal, setShowImageModal] = useState(false);
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const [loading, setLoading] = useState(true);
+
   // Fetch verified handymen from the backend
   useEffect(() => {
-    axios
-      .get(
-        "https://e-handyhelp-web-backend.onrender.com/api/handymen/verified"
-      )
-      .then((response) => {
-        setVerifiedHandymen(response.data);
-      })
-      .catch((error) => {
+    const fetchVerifiedHandymen = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          "https://e-handyhelp-web-backend.onrender.com/api/handymen/verified"
+        );
+        const sortedHandyman = response.data.sort((a, b) => {
+          return (
+            new Date(b.verifiedAt || b.updatedAt || b.createdAt) -
+            new Date(a.verifiedAt || a.updatedAt || a.createdAt)
+          );
+        });
+        setVerifiedHandymen(sortedHandyman);
+      } catch (error) {
         console.error("Error fetching verified handymen:", error);
-      });
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchVerifiedHandymen();
   }, []);
+
+  const customStyles = {
+    headCells: {
+      style: {
+        fontWeight: "bold",
+        fontSize: "16px",
+      },
+    },
+  };
 
   const handleOpenModal = (handyman) => {
     setSelectedHandyman(handyman);
@@ -41,11 +66,6 @@ const VerifiedHandyman = () => {
     setShowConfirmDelete(true);
   };
 
-  const handleCloseDeleteModal = () => {
-    setShowConfirmDelete(false);
-    setSelectedHandyman(null);
-  };
-
   const handleDeleteHandyman = async () => {
     if (selectedHandyman) {
       try {
@@ -57,14 +77,12 @@ const VerifiedHandyman = () => {
             (handyman) => handyman._id !== selectedHandyman._id
           )
         );
-        setAlert({message: "Handyman deleted successfully!"});
-        
+        setAlert({ message: "Handyman deleted successfully!" });
       } catch (error) {
         console.error("Error deleting handyman:", error);
       } finally {
         setShowConfirmDelete(false);
         setSelectedHandyman(null);
-        
       }
     }
   };
@@ -83,34 +101,24 @@ const VerifiedHandyman = () => {
     {
       name: "Name",
       selector: (row) => `${row.fname} ${row.lname}`,
-      sortable: true,
     },
     {
       name: "Username",
       selector: (row) => row.username,
-      sortable: true,
     },
     {
       name: "Account Status",
       selector: (row) => row.accounts_status || "Verified Handyman",
-      sortable: true,
     },
     {
       name: "Action",
       cell: (row) => (
         <div className="button-group">
-          <Button 
-          onClick={() => handleOpenModal(row)}>
-            Details
-          </Button>
-          <Button
-            
-            onClick={() => handleOpenDeleteModal(row)}
-            className="ml-2"
-          >
+          <Button onClick={() => handleOpenModal(row)}>Details</Button>
+          <Button onClick={() => handleOpenDeleteModal(row)} className="ml-2">
             Delete
           </Button>
-          </div>
+        </div>
       ),
     },
   ];
@@ -133,16 +141,18 @@ const VerifiedHandyman = () => {
         striped
         responsive
         style={{ minHeight: "400px" }} // Consistent with other component layout
+        progressPending={loading}
+        customStyles={customStyles}
       />
 
-          {/* Alert for success or error messages */}
-             {alert && (
-              <Alert variant={alert.type} onClose={() => setAlert(null)} dismissible>
-                {alert.message}
-              </Alert>
-            )}
+      {/* Alert for success or error messages */}
+      {alert && (
+        <Alert variant={alert.type} onClose={() => setAlert(null)} dismissible>
+          {alert.message}
+        </Alert>
+      )}
 
-      {/* Modal for handyman details */}
+      {/* Modal for Handyman details */}
       <Modal show={showModal} onHide={handleCloseModal} centered>
         <Modal.Header style={{ backgroundColor: "#1960b2" }} closeButton>
           <Modal.Title>Handyman Details</Modal.Title>
@@ -153,47 +163,116 @@ const VerifiedHandyman = () => {
               <h5>
                 Name: {selectedHandyman.fname} {selectedHandyman.lname}
               </h5>
-              <p>Username: {selectedHandyman.username}</p>
-              <p>
-                Description:{" "}
-                {selectedHandyman.accounts_status || "Verified Handyman"}
-              </p>
+              <p>Address: {selectedHandyman.address}</p>
               <p>Email: {selectedHandyman.email}</p>
+              <p>Username: {selectedHandyman.username}</p>
+              <p>Contact: {selectedHandyman.contact}</p>
               <p>
-                Specialization: {selectedHandyman.specialization.join(", ")}
+                Specialization:{" "}
+                {Array.isArray(selectedHandyman.specialization)
+                  ? selectedHandyman.specialization.join(", ")
+                  : selectedHandyman.specialization}
               </p>
+
+              <p>
+                Date of Birth:{" "}
+                {new Date(selectedHandyman.dateOfBirth).toLocaleDateString()}
+              </p>
+              {selectedHandyman.images && selectedHandyman.images.length > 0 ? (
+                <>
+                  <strong>Valid ID:</strong>
+                  <div className="image-carousel-handyman">
+                    <button
+                      className="carousel-btn-handyman left"
+                      onClick={() =>
+                        setImageIndex((prev) =>
+                          prev > 0
+                            ? prev - 1
+                            : selectedHandyman.images.length - 1
+                        )
+                      }
+                    >
+                      &#10094;
+                    </button>
+                    <img
+                      src={
+                        selectedHandyman.images[imageIndex].startsWith(
+                          "data:image"
+                        )
+                          ? selectedHandyman.images[imageIndex]
+                          : `data:image/png;base64,${selectedHandyman.images[imageIndex]}`
+                      }
+                      alt={`Valid ID ${imageIndex + 1}`}
+                      className="carousel-image-handyman fixed-size"
+                      onClick={() => setShowImageModal(true)}
+                    />
+                    <button
+                      className="carousel-btn-handyman right"
+                      onClick={() =>
+                        setImageIndex((prev) =>
+                          prev < selectedHandyman.images.length - 1
+                            ? prev + 1
+                            : 0
+                        )
+                      }
+                    >
+                      &#10095;
+                    </button>
+                  </div>
+
+                  {/* Image Modal for Full-Size View */}
+                  {showImageModal && (
+                    <div
+                      className="image-modal-handyman"
+                      onClick={() => setShowImageModal(false)}
+                    >
+                      <div className="modal-content-handyman">
+                        <img
+                          src={
+                            selectedHandyman.images[imageIndex].startsWith(
+                              "data:image"
+                            )
+                              ? selectedHandyman.images[imageIndex]
+                              : `data:image/png;base64,${selectedHandyman.images[imageIndex]}`
+                          }
+                          alt={`Valid ID ${imageIndex + 1}`}
+                          className="full-size-image-handyman"
+                        />
+                      </div>
+                    </div>
+                  )}
+                </>
+              ) : (
+                <p>
+                  <strong>Valid ID:</strong> <em>No ID provided</em>
+                </p>
+              )}
             </>
           )}
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={handleCloseModal}>
-            Close
-          </Button>
+          <Button onClick={handleCloseModal}>Close</Button>
         </Modal.Footer>
       </Modal>
 
-      {/* Modal for delete confirmation */}
-      <Modal show={showConfirmDelete} onHide={handleCloseDeleteModal} centered>
+      {/* Confirmation Modal for Deletion */}
+      <Modal
+        show={showConfirmDelete}
+        onHide={() => setShowConfirmDelete(false)}
+        centered
+      >
         <Modal.Header style={{ backgroundColor: "#1960b2" }} closeButton>
           <Modal.Title>Confirm Deletion</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Are you sure you want to delete the handyman{" "}
-          <strong>
-            {selectedHandyman?.fname} {selectedHandyman?.lname}
-          </strong>
-          ?
+          Are you sure you want to delete {selectedHandyman?.fname}{" "}
+          {selectedHandyman?.lname}?
         </Modal.Body>
         <Modal.Footer>
-          <Button  onClick={handleCloseDeleteModal}>
-            Cancel
-          </Button>
-          <Button  onClick={handleDeleteHandyman}>
-            Delete
-          </Button>
+          <Button onClick={() => setShowConfirmDelete(false)}>Cancel</Button>
+          <Button onClick={handleDeleteHandyman}>Delete</Button>
         </Modal.Footer>
       </Modal>
-
     </div>
   );
 };
