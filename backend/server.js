41f0356e891ceb22672a08fb1ba1ec0e0da73a4c
Jherick Timban <jrickvillaqwe@gmail.com>
2025-03-06 11:20:54 +0800

    updated files

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const handymenRoute = require("./routes/handymen");
 const dashboardRoute = require("./routes/dashboard");
 const reportRoute = require("./routes/reports");
 const feedbackRoute = require("./routes/feedbacks");
-const activityLogRoute = require("./routes/activitylogs");
+const activityLogRoute = require("./routes/activityLogs");
 
 const app = express();
 
